1b0d5bbcbe1bf9081d7d2ce7e9ee05e89808b7ae
Ashikur Rahman <113477733+ashikur-rahman-fh@users.noreply.github.com>
2023-06-01 13:48:10 +0000

    Add runner shell script

---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]; then
+    echo "Fetal error: Filename required."
+    exit 1
+fi
+filename=$1
+
+inputfile=$2
+
+if [[ ! -f $filename ]]; then
+    echo "Error: File expected."
+    exit 1
+fi
+
+if [[ ! -f sol.exe || $(date -r $filename) > $(date -r sol.exe) ]]; then
+    echo "Building $filename ..."
+    g++ $filename -std=c++17 -Wall -o sol.exe -fmax-errors=2
+
+else
+    echo "$filename is not modified since the last build"
+fi
+
+if [[ $? -eq 0 ]]; then
+    echo "Executing $filename"
+    echo "----------------------------------------------------"
+    time timeout 2 ./sol.exe
+fi
+
+exitcode=$?
+
+# new line for good formatting
+echo
+
+if [[ exitcode -eq 124 ]]; then
+    echo "Time limit exeeded."
+elif [[ exitcode -eq 0 ]]; then
+    echo "Successful."
+else
+    echo "Runtime error."
+fi
---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    cout << "Hello " << str << "\n";
+    cout << "Just a test file\n";
+    // while (true) {
+    //     assert(false);
+    // }
+    return 0;
+}
