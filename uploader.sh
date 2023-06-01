92eb256d0bfb36eb1bc4ac6de70c6015566dc236
Ashikur Rahman <113477733+ashikur-rahman-fh@users.noreply.github.com>
2023-06-01 18:36:41 +0000

    Update uploader script

--- uploader.sh
+++ uploader.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
+dateValue=$(date +'%Y-%m-%d')
+commitMessage="Changes from $dateValue"
 
-uploadTime=15
-while [[ true ]];
-do
-  now=$(date +'%H')
-
-  if [[ now -eq $uploadTime ]]; then
-    dateValue=$(date +'%Y-%m-%d')
-    commitMessage="Changes from $dateValue"
-    
-    git add . > /dev/null
-    git commit -m "$commitMessage" > /dev/null
-    git push origin master > /dev/null
-  fi
-
-  # sleep for half an hour!
-  sleep 1800
-done
+git add . > /dev/null
+git commit -m "$commitMessage" > /dev/null
+git push origin master > /dev/null
